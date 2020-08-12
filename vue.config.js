f5cdec43afc5e5e3fda131fd5fd489f0c8b8e7d0
XDXT <1006334837@qq.com>
2020-08-12 18:52:48 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+
 module.exports = {
-  publicPath: '/chatapp/',
-  productionSourceMap: false
+  publicPath: process.env.NODE_ENV === 'production' ? '/chatapp/' : '/',
+  productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 生产环境
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: /\.(js|css)$/,
+          threshold: 10240,
+          minRatio: 0.8
+        })
+      );
+    } else {
+      // 开发环境
+    }
+  }
 }
